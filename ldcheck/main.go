b6be62039f26ccbff6ad0fac8f44f38b18f3938b
Romain Doumenc <romain@trout.software>
2023-04-27 11:42:40 +0100

    Allow older TLS version for LDAP

--- ldcheck/main.go
+++ ldcheck/main.go
@@ -4,9 +4,13 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"net"
+	"net/url"
 	"os"
 	"strings"
 	"text/template"
+
+	"crypto/tls"
 
 	"github.com/BurntSushi/toml"
 	"github.com/go-ldap/ldap/v3"
@@ -19,7 +23,7 @@ func TestLoginWithLDAP(url, query, name, pass string) {
 		log.Fatal(err, "invalid bind pattern, no access will ever match")
 	}
 
-	ctn, err := ldap.DialURL(url)
+	ctn, err := DialURL(url)
 	if err != nil {
 		log.Fatal(err, "cannot contact LDAP server")
 	}
@@ -83,4 +87,42 @@ func main() {
 	}
 
 	TestLoginWithLDAP(Config.LDAP.ServerURL, Config.LDAP.BindUserPattern, *name, *pass)
+}
+
+func DialURL(addr string) (*ldap.Conn, error) {
+	lurl, err := url.Parse(addr)
+	if err != nil {
+		return nil, ldap.NewError(ldap.ErrorNetwork, err)
+	}
+
+	host, port, err := net.SplitHostPort(lurl.Host)
+	if err != nil {
+		// we asume that error is due to missing port
+		host = lurl.Host
+		port = ""
+	}
+
+	switch lurl.Scheme {
+	case "ldapi":
+		if lurl.Path == "" || lurl.Path == "/" {
+			lurl.Path = "/var/run/slapd/ldapi"
+		}
+		return ldap.Dial("unix", lurl.Path)
+	case "ldap":
+		if port == "" {
+			port = ldap.DefaultLdapPort
+		}
+		return ldap.Dial("tcp", net.JoinHostPort(host, port))
+	case "ldaps":
+		if port == "" {
+			port = ldap.DefaultLdapsPort
+		}
+		tlsConf := &tls.Config{
+			ServerName: host,
+			MinVersion: tls.VersionTLS10,
+		}
+		return ldap.DialTLS("tcp", net.JoinHostPort(host, port), tlsConf)
+	}
+
+	return nil, ldap.NewError(ldap.ErrorNetwork, fmt.Errorf("Unknown scheme '%s'", lurl.Scheme))
 }
